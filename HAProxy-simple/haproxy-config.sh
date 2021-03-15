9d5bee5c6c12c02bb51dce348b5096b771f55967
carban <murillo.carlos@correounivalle.edu.co>
2021-03-15 15:45:55 -0500

    init

---
+++ HAProxy-simple/haproxy-config.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+sudo apt install haproxy -y && 
+echo "
+global
+        log /dev/log   local0
+        log 127.0.0.1   local1 notice
+        maxconn 4096
+        user haproxy
+        group haproxy
+        daemon
+
+defaults
+        log     global
+        mode    http
+        option  httplog
+        option  dontlognull
+        retries 3
+        option redispatch
+        maxconn 2000
+        contimeout     5000
+        clitimeout     50000
+        srvtimeout     50000
+
+listen webfarm 
+bind 0.0.0.0:80
+    mode http
+    stats enable
+    stats uri /haproxy?stats
+    balance roundrobin
+    option httpclose
+    option forwardfor
+    server web1 192.168.33.11:80 check
+    server web2 192.168.33.12:80 check
+" > /etc/haproxy/haproxy.cfg && sudo service haproxy restart
---
+++ hello-world/script.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install htop apache2 -y
